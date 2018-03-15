7dd400d82719143e3cd49c04557dcd6820268405
sujian <121116111@qq.com>
2018-03-15 17:17:04 +0800

    update:阿里推送部分

--- android/app/src/main/java/com/react_native_study/MainApplication.java
+++ android/app/src/main/java/com/react_native_study/MainApplication.java
@@ -2,48 +2,79 @@ package com.react_native_study;
 
 import android.app.Application;
 
+import com.alibaba.sdk.android.push.CommonCallback;
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
 import com.facebook.react.ReactApplication;
+
 import io.realm.react.RealmReactPackage;
+
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+
 import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage(),
+                    new RealmReactPackage(),
+                    new VectorIconsPackage(),
+                    new PushPackage()
+            );
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RealmReactPackage(),
-          new VectorIconsPackage()
-      );
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
+        this.initCloudChannel();
     }
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
+
+    private void initCloudChannel() {
+        PushServiceFactory.init(this.getApplicationContext());
+        PushServiceFactory.getCloudPushService().register(this.getApplicationContext(), new CommonCallback() {
+            @Override
+            public void onSuccess(String s) {
+                WritableMap params = Arguments.createMap();
+                params.putBoolean("success", true);
+                PushModule.sendEvent("onInit", params);
+            }
+
+            @Override
+            public void onFailed(String s, String s1) {
+                WritableMap params = Arguments.createMap();
+                params.putBoolean("success", false);
+                params.putString("errorMsg", "errorCode:" + s + ". errorMsg:" + s1);
+                PushModule.sendEvent("onInit", params);
+            }
+        });
     }
-  };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
---
+++ android/app/src/main/java/com/react_native_study/PushPackage.java
@@ -0,0 +1,32 @@
+package com.react_native_study;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * Created by hns on 2018/3/15.
+ */
+
+public class PushPackage implements ReactPackage {
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new PushModule(reactContext));
+        return modules;
+    }
+
+
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+}
